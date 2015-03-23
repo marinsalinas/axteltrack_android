75e02566c3ecd03fb62232de4fdd2b077562c989
Marín Salinas <marinssalinas@gmail.com>
2015-03-23 17:27:00 -0600

    add permission to manifest and SplashScreen Session login

--- app/src/main/java/mx/axtel/connectedcar/SplashScreen.java
+++ app/src/main/java/mx/axtel/connectedcar/SplashScreen.java
@@ -1,42 +1,91 @@
 package mx.axtel.connectedcar;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import mx.axtel.connectedcar.helpers.Session;
+import mx.axtel.connectedcar.models.User;
 
 
 public class SplashScreen extends ActionBarActivity {
-
+    Session session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         //setToolbar();
 
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                return null;
-            }
+       if(session == null){
+           session = new Session(getApplicationContext());
+       }
 
 
-            @Override
-            protected void onPostExecute(Void result){
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
-            }
-        }.execute();
+       if(session.isLoggedIn() && isOnline()){
+           RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+           //Post Params.
+           Map<String, String> params = new HashMap<String, String>();
+           params.put("account", session.getUserSession().getAccount());
+           params.put("user", session.getUserSession().getUser());
+           params.put("password", session.getUserSession().getPassword());
+
+           String URL =  getResources().getString(R.string.auth_login);
+
+           JsonObjectRequest req = new JsonObjectRequest(
+                   Request.Method.POST,
+                   URL,
+                   new JSONObject(params),
+                   new Response.Listener<JSONObject>() {
+                       @Override
+                       public void onResponse(JSONObject response) {
+                           Gson gson  = new Gson();
+                           session.createLoginSession(gson.fromJson(response.toString(), User.class));
+                           startActivity(new Intent(getApplicationContext(), MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                           finish();
+                       }
+                   },
+                   new Response.ErrorListener() {
+                       @Override
+                       public void onErrorResponse(VolleyError error) {
+                           startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                           finish();
+                       }
+                   }) {
+               //Configurando Headers para que tome JSON
+               @Override
+               public Map<String, String> getHeaders() throws AuthFailureError {
+                   HashMap<String, String> headers = new HashMap<String, String>();
+                   headers.put("Content-Type", "application/json; charset=utf-8");
+                   return headers;
+               }
+           };
+           queue.add(req);
+       }else{
+           startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+           finish();
+       }
 
 
     }
@@ -45,6 +94,13 @@ public class SplashScreen extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_my_toolbar);
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
+    }
+
+    public  boolean isOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getApplicationContext().getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 
 }
